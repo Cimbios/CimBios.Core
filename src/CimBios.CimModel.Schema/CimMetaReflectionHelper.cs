e55ba8c143eeadf56a8b24ffde3d28e9969c44d5
Юрий Коваленко <belizahrt@live.com>
2024-08-04 11:59:31 +1000

    Добавьте файлы проекта.

---
+++ src/CimBios.CimModel.Schema/CimMetaReflectionHelper.cs
@@ -0,0 +1,92 @@
+﻿using System.Reflection;
+
+namespace CimBios.CimModel.Schema
+{
+    internal class CimSchemaReflectionHelper
+    {
+        public CimSchemaReflectionHelper()
+        {
+            CollectSerializableTypes();
+        }
+
+        public bool TryGetTypeInfo(string uri, out TypeInfo? typeInfo)
+        {
+            return _Types.TryGetValue(uri, out typeInfo);
+        }
+
+        public bool TryGetMemberInfo(string uri, out MemberInfo? memberInfo)
+        {
+            return _Members.TryGetValue(uri, out memberInfo);
+        }
+
+        private void CollectSerializableTypes()
+        {
+            _Types.Clear();
+            _Members.Clear();
+
+            foreach (var type in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                var typeInfo = type.GetTypeInfo();
+                if (typeInfo.IsDefined(typeof(CimSchemaSerializableAttribute), true) == false)
+                {
+                    continue;
+                }
+
+                var attribute = typeInfo.GetCustomAttribute<CimSchemaSerializableAttribute>(false);
+                if (attribute != null)
+                {
+                    _Types.TryAdd(attribute.AbsoluteUri, type.GetTypeInfo());
+
+                    CollectSerializableMembers(type.GetTypeInfo());
+                }
+            }
+        }
+
+        private void CollectSerializableMembers(TypeInfo typeInfo)
+        {
+            foreach (var property in typeInfo.GetMembers())
+            {
+                if (property.IsDefined(typeof(CimSchemaSerializableAttribute), false) == false)
+                {
+                    continue;
+                }
+
+                var attribute = property.GetCustomAttribute<CimSchemaSerializableAttribute>(false);
+                if (attribute != null)
+                {
+                    _Members.TryAdd(attribute.AbsoluteUri, property);
+                }
+            }
+        }
+
+        public void SetMetaMemberValue<T>(object descriptionClass,
+            MemberInfo member, Attribute attribute, T value)
+        {
+            if (member is PropertyInfo propertyInfo)
+            {
+                if (propertyInfo.GetValue(descriptionClass) is ICollection<T> collection)
+                {
+                    collection.Add(value);
+                }
+                else
+                {
+                    propertyInfo.SetValue(descriptionClass, value);
+                }
+            }
+        }
+
+        private Dictionary<string, TypeInfo> _Types { get; }
+            = new Dictionary<string, TypeInfo>();
+        private Dictionary<string, MemberInfo> _Members { get; }
+            = new Dictionary<string, MemberInfo>();
+
+    }
+
+    internal enum MetaFieldType
+    {
+        Value,
+        Enum,
+        ByRef,
+        Datatype,
+    }
+}
---
+++ src/CimBios.RdfXmlReader/RdfXmlUtils.cs
@@ -0,0 +1,99 @@
+﻿using System.Xml.Linq;
+
+namespace CimBios.RdfXml.IOLib
+{
+    /// <summary>
+    /// Flat structured RDF node class.
+    /// </summary>
+    public class RdfNode
+    {
+        public RdfNode(Uri identifier, XElement element,
+            RdfTriple[] triples, bool isAuto)
+        {
+            Identifier = identifier;
+            Element = element;
+            Triples = triples;
+            IsAuto = isAuto;
+        }
+
+        public Uri Identifier { get; set; }
+        public XElement Element { get; set; }
+        public RdfTriple[] Triples { get; set; }
+        public bool IsAuto { get; set; } = false;
+    }
+
+    /// <summary>
+    /// RDF-Triple class.
+    /// </summary>
+    public class RdfTriple
+    {
+        public RdfTriple(Uri subject, Uri predicate,
+            object @object)
+        {
+            Subject = subject;
+            Predicate = predicate;
+            Object = @object;
+        }
+
+        public Uri Subject { get; set; }
+
+        /// <summary>
+        /// Uri type limited predicate.
+        /// </summary>
+        public Uri Predicate { get; set; }
+
+        public object Object { get; set; }
+    }
+
+    /// <summary>
+    /// Helper class for rdf entities.
+    /// </summary>
+    public static class RdfXmlReaderUtils
+    {
+        public static T? ExtractPredicateValue<T>(RdfNode node,
+            Uri predicate) where T : class
+        {
+            var triples = node.Triples
+                .Where(t => RdfUriEquals(t.Predicate, predicate));
+            if (triples.Count() > 0 && triples.First().Object is T value)
+            {
+                return value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Equality respects URI fragments comparision.
+        /// </summary>
+        public static bool RdfUriEquals(Uri lUri, Uri rUri)
+        {
+            return lUri.AbsoluteUri == rUri.AbsoluteUri;
+        }
+    }
+
+    public class RdfUriComparer : EqualityComparer<Uri>
+    {
+        public override bool Equals(Uri? lUri, Uri? rUri)
+        {
+            if (lUri != null && rUri != null)
+            {
+                return RdfXmlReaderUtils.RdfUriEquals(lUri, rUri);
+            }
+
+            if (lUri == null && rUri == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode(Uri uri)
+        {
+            return uri.AbsoluteUri.GetHashCode();
+        }
+    }
+}
