87524a06f9703ae0f2a122ae83afab77d1232978
belizahrt <belizahrt@gmail.com>
2024-08-19 22:18:09 +1000

    Init RdfXmlFileDataProvider * using TextReader 4 test

--- src/Core/DataProvider/DataProviderBase.cs
+++ src/Core/DataProvider/DataProviderBase.cs
@@ -1,6 +1,43 @@
-﻿namespace CimBios.Core.DataProvider;
+﻿using System.Xml.Linq;
 
-public abstract class DataProviderBase
+namespace CimBios.Core.DataProvider;
+
+public abstract class DataProviderBase<T>
+    where T : class
 {
+    public Uri Source { get; set; }
 
+    protected DataProviderBase(Uri source)
+    {
+        Source = source;
+    }
+
+    public abstract T Get();
+    public abstract void Push(T data);
+}
+
+public class RdfXmlFileDataProvider : DataProviderBase<TextReader>
+{
+    public RdfXmlFileDataProvider(Uri source) : base(source)
+    {
+    }
+
+    public override TextReader Get()
+    {
+        return new StreamReader(Source.LocalPath);
+    }
+
+    public override void Push(TextReader data)
+    {
+        using (data)
+        using (StreamWriter sw = File.CreateText(Source.LocalPath))
+        {
+            string? line = data.ReadLine(); 
+            while (line != null)         
+            {
+                Console.WriteLine(line);
+                line = data.ReadLine();
+            }
+        }
+    }
 }
