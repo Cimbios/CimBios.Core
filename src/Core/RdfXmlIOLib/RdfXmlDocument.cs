d3962dd0f6fff7b39405b43bd4a93281dddd3614
belizahrt <belizahrt@gmail.com>
2024-08-19 22:30:33 +1000

    RdfXmlDocument init

---
+++ src/Core/RdfXmlIOLib/RdfXmlDocument.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CimBios.Core.RdfXmlIOLib;
+
+public class RdfXmlDocument
+{
+    public RdfXmlDocument()
+    {
+    }
+
+    public RdfXmlDocument(string path)
+    {
+    }
+}
