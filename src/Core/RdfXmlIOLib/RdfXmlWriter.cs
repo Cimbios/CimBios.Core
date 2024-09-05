5b59017c73be58af55750ef2cb0f8d45acbd97be
belizahrt <belizahrt@gmail.com>
2024-09-05 14:36:05 +1000

    TODO added

--- src/Core/RdfXmlIOLib/RdfXmlWriter.cs
+++ src/Core/RdfXmlIOLib/RdfXmlWriter.cs
@@ -1,24 +1,63 @@
 ﻿using System.Xml.Linq;
 
 namespace CimBios.Core.RdfXmlIOLib;
+
+// TODO: docs everywhere
+
+/// <summary>
+/// 
+/// </summary>
 public class RdfXmlWriter
 {
+    // TODO:    Code style fields order: props (public, private), constructor, 
+    //          methods (public, private), fields
+    //          + lines 80 chars length limit!
+    
+    // TODO: no property? needs only in Write method.
     private XDocument _xDoc;
     public RdfXmlWriter() { }
+
+    /// <summary>
+    /// RDF document namespaces dictionary.
+    /// </summary>
+    /// TODO: Fill it from ICimSchema.Namespaces via serializer. 
+    public Dictionary<string, XNamespace> Namespaces { get => _Namespaces; }
+
+    private Dictionary<string, XNamespace> _Namespaces { get; set; }
+        = new Dictionary<string, XNamespace>();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="rdfNodes"></param>
+    /// <returns></returns>
     public XDocument Write(IEnumerable<RdfNode> rdfNodes)
     {
         _xDoc = new XDocument();
-        WriteFirstLine();
+
+        // TODO: add root xelement to xdoc here
+        CreateRootRdfNode();
+        
         foreach (RdfNode rdfNode in rdfNodes)
         {
+            // TODO: exlude '#_' prefix
             var serializedNode = new XElement(rdfNode.TypeIdentifier.ToString(), new XAttribute("rdf:about", $"#_{rdfNode.Identifier}"));
             WriteTriples(ref serializedNode, rdfNode.Triples);
 
             _xDoc.Add(serializedNode);
         }
+
         return _xDoc;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="serializedNode"></param>
+    /// <param name="triples"></param>
+    /// TODO:   Exlude System.Guid parsing
+    ///         Add rdf:id support
+    ///         Exlude '#_' prefix (its serializer resp-ty)
     private void WriteTriples(ref XElement serializedNode, IEnumerable<RdfTriple> triples)
     {
         foreach (var triple in triples)
@@ -27,9 +66,8 @@ public class RdfXmlWriter
             {
                 serializedNode.Add(new XElement(triple.Predicate.ToString(), new XAttribute("rdf:resource", $"#_{guidResult}")));
             }
-            else if (triple.Object is RdfNode)
+            else if (triple.Object is RdfNode compound)
             {
-                var compound = (RdfNode)triple.Object;
                 var compoundNode = new XElement(triple.Predicate.ToString(),
                                        new XElement(compound.TypeIdentifier.ToString()));
                 WriteTriples(ref compoundNode, compound.Triples); // Рекурсия для компаундов
@@ -39,7 +77,12 @@ public class RdfXmlWriter
         }
     }
 
-    private void WriteFirstLine()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// TODO:   reimplement namespaces to Namespaces property.
+    ///         return xelement besides to add to xdoc.
+    private void CreateRootRdfNode()
     {
         XAttribute[] namespaces = 
             {   new XAttribute("xmlns:rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
