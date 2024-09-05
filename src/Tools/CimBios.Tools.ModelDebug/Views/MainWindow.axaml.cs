4c1144fb70f1dc9b2484e0d32cb2b119137ef583
belizahrt <belizahrt@gmail.com>
2024-09-05 15:22:31 +1000

    init avalonia proj

---
+++ src/Tools/CimBios.Tools.ModelDebug/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CimBios.Tools.ModelDebug.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
