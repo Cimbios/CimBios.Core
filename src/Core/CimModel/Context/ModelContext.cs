055aadfca12a81834077c9eab529a8c57522e4a4
belizahrt <belizahrt@gmail.com>
2024-09-05 14:13:00 +1000

    Save methods added + Minor improvments: -Remove object method added -Docs added

--- src/Core/CimModel/Context/ModelContext.cs
+++ src/Core/CimModel/Context/ModelContext.cs
@@ -12,10 +12,14 @@ namespace CimBios.Core.CimModel.Context;
 /// </summary>
 public class ModelContext
 {
+    /// <summary>
+    /// Model description.
+    /// </summary>
     public IFullModel? Description { get; set; }
 
-    public ContextSettings Settings { get; set; } = new ContextSettings();
-
+    /// <summary>
+    /// All cached objects collection (uuid to IModelObject).
+    /// </summary>
     private Dictionary<string, IModelObject> _Objects { get; set; }
 
     public ModelContext()
@@ -28,6 +32,9 @@ public class ModelContext
         InitContextDataConfig(contextDataConfig);
     }
 
+    /// <summary>
+    /// Load CIM model to context via config.
+    /// </summary>
     public void Load()
     {
         if (_provider == null || _serializer == null)
@@ -35,22 +42,18 @@ public class ModelContext
             return;
         }
 
-        _serializer.Settings.AllowUnkownClassProperties = false;
         var serialized = _serializer.Deserialize();
 
         _Objects = new Dictionary<string, IModelObject>(serialized
             .Select(x => new KeyValuePair<string, IModelObject>(x.Uuid, x)));
 
-        var fullModel = _Objects.Values.OfType<IFullModel>()
-            .FirstOrDefault();
-
-        if (fullModel != null)
-        {
-            Description = fullModel;
-            _Objects.Remove(fullModel.Uuid);
-        }
+        ReadModelDescription();
     }
 
+    /// <summary>
+    /// Load CIM model to context via config.
+    /// </summary>
+    /// <param name="contextDataConfig">Context configuration.</param>
     public void Load(IModelContextConfig contextDataConfig)
     {
         InitContextDataConfig(contextDataConfig);
@@ -58,11 +61,20 @@ public class ModelContext
         Load();
     }
 
+    /// <summary>
+    /// Get all model objects.
+    /// </summary>
+    /// <returns>IModelObject instance collection.</returns>
     public IEnumerable<IModelObject> GetAllObjects()
     {
         return _Objects.ToList().Select(kvp => kvp.Value);
     }
 
+    /// <summary>
+    /// Get generalized model object by uuid.
+    /// </summary>
+    /// <param name="uuid"></param>
+    /// <returns>IModelObject instance or null.</returns>
     public IModelObject? GetObject(string uuid)
     {
         if (_Objects.TryGetValue(uuid, out var instance)
@@ -80,6 +92,12 @@ public class ModelContext
         }
     }
 
+    /// <summary>
+    /// Get typed model object by uuid.
+    /// </summary>
+    /// <typeparam name="T">IModelObject generalized class.</typeparam>
+    /// <param name="uuid">Model object string identifier.</param>
+    /// <returns>T casted IModelObject instance or null.</returns>
     public T? GetObject<T>(string uuid) where T : IModelObject
     {
         IModelObject? modelObject = GetObject(uuid);
@@ -91,12 +109,31 @@ public class ModelContext
         return default;
     }
 
+    /// <summary>
+    /// Remove object from model context.
+    /// </summary>
+    /// <param name="uuid">Model object string identifier.</param>
+    /// <returns>True if object found and removed.</returns>
+    public bool RemoveObject(string uuid)
+    {
+        return _Objects.Remove(uuid);
+    }
+
+    /// <summary>
+    /// Fires on IModelObject property changed.
+    /// </summary>
+    /// <param name="sender">Firing IModelObject instance.</param>
+    /// <param name="e">Changed property info.</param>
     private void Notify_ModelObjectPropertyChanged(object? sender, 
         PropertyChangedEventArgs e)
     {
     //    throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Initialize read/write model stategy.
+    /// </summary>
+    /// <param name="contextDataConfig">Context configuration.</param>
     private void InitContextDataConfig(IModelContextConfig contextDataConfig)
     {
         _provider = contextDataConfig.DataProvider;
@@ -105,13 +142,21 @@ public class ModelContext
         _serializer.Schema = contextDataConfig.CimSchema;
     }
 
+    /// <summary>
+    /// Find and extract IFullModel description from _objects cache.
+    /// </summary>
+    private void ReadModelDescription()
+    {
+        var fullModel = _Objects.Values.OfType<IFullModel>()
+            .FirstOrDefault();
+
+        if (fullModel != null)
+        {
+            Description = fullModel;
+            _Objects.Remove(fullModel.Uuid);
+        }
+    }
+
     private IDataProvider? _provider;
     private RdfSerializerBase? _serializer;
-}
-
-public class ContextSettings
-{
-    public bool AllowUnkownClassTypes { get; set; } = true;
-    public bool AllowUnkownClassProperties { get; set; } = true;
-    public bool AllowUriPathMismatches { get; set; } = true;
 }
