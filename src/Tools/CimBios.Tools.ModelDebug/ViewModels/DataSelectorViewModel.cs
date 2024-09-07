13f1160dec2507fc45db29f6876b403180f0ea91
belizahrt <belizahrt@gmail.com>
2024-09-07 16:32:41 +1000

    Data selector mvvm implemented

---
+++ src/Tools/CimBios.Tools.ModelDebug/ViewModels/DataSelectorViewModel.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using CimBios.Core.CimModel.Schema.RdfSchema;
+using CimBios.Core.DataProvider;
+using CimBios.Tools.ModelDebug.Models;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CimBios.Tools.ModelDebug.ViewModels;
+
+public class DataSelectorViewModel : ViewModelBase, 
+    INotifyPropertyChanged
+{
+    public ObservableCollection<DataProviderModel> DataProviders { get; }
+    public ObservableCollection<SchemaSelectorModel> Schemas { get; }
+    public DataProviderModel? SelectedDataProvider { get; set; }
+    public SchemaSelectorModel? SelectedSchema { get; set; }
+    public AsyncRelayCommand ShowProviderSourceSelectorCommand { get; }
+    public AsyncRelayCommand ShowSchemaSourceSelectorCommand { get; }
+
+    public string SourceStringUri 
+    { 
+        get => SourceUri != null ? SourceUri.AbsoluteUri : string.Empty;
+    }
+
+    public string SchemasStringUri 
+    { 
+        get
+        {
+            if (SchemasUri != null)
+            {
+                return string.Join(";", SchemasUri
+                    .Select(s => $"\"{s.AbsoluteUri}\""));
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    private Uri? SourceUri 
+    {
+        get => _sourceUri;
+        set
+        {
+            _sourceUri = value;
+            OnPropertyChanged(nameof(SourceStringUri));       
+        }
+    }
+
+    private IEnumerable<Uri>? SchemasUri
+    {
+        get => _schemasUri;
+        set
+        {
+            _schemasUri = value;
+            OnPropertyChanged(nameof(SchemasStringUri));       
+        }
+    }
+
+    private Window? MainWindow 
+    { 
+        get
+        {
+            if (Application.Current?.ApplicationLifetime 
+                is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                return desktop.MainWindow;
+            }
+
+            return null;
+        }
+    }
+
+    public DataSelectorViewModel()
+    {
+        DataProviders = new ObservableCollection<DataProviderModel>()
+        {
+            new DataProviderModel("CIMXML File", 
+                new RdfXmlFileDataProviderFactory(),
+                new FileDialogSourceSelector() { OwnerWindow =  MainWindow }),
+        };
+        SelectedDataProvider = DataProviders.FirstOrDefault();
+
+        Schemas = new ObservableCollection<SchemaSelectorModel>()
+        {
+            new SchemaSelectorModel("RDFS",
+                new CimRdfSchemaFactory(),
+                new FileDialogSourceSelector() 
+                    { OwnerWindow =  MainWindow, MultiSelect = true }),
+        };
+        SelectedSchema = Schemas.FirstOrDefault();
+
+        ShowProviderSourceSelectorCommand = 
+            new AsyncRelayCommand(ShowProviderSourceSelector);
+
+        ShowSchemaSourceSelectorCommand = 
+            new AsyncRelayCommand(ShowSchemaSourceSelector);
+    }
+
+    protected new virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private async Task ShowProviderSourceSelector()
+    {
+        if (SelectedDataProvider != null)
+        {
+            var sources = await GetSourceList(
+                SelectedDataProvider.SourceSelector);
+            
+            if (sources != null)
+            {
+                SourceUri = sources.FirstOrDefault();
+            }
+        }
+    }
+
+    private async Task ShowSchemaSourceSelector()
+    {
+        if (SelectedSchema != null)
+        {
+            var sources = await GetSourceList(
+                SelectedSchema.SourceSelector);
+            
+            if (sources != null)
+            {
+                SchemasUri = sources;
+            }
+        }
+    }
+
+    private async Task<IEnumerable<Uri>> GetSourceList(
+        ISourceSelector sourceSelector)
+    {
+        var result = await sourceSelector.GetSourceAsync();
+        if (result != null)
+        {
+            return result;
+        }
+
+        return new List<Uri>();
+    }
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
+
+    private Uri? _sourceUri;
+    private IEnumerable<Uri>? _schemasUri;
+}
--- src/Tools/CimBios.Tools.ModelDebug/ViewModels/MainWindowViewModel.cs
+++ src/Tools/CimBios.Tools.ModelDebug/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,49 @@
-﻿namespace CimBios.Tools.ModelDebug.ViewModels;
+﻿using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using CimBios.Tools.ModelDebug.Views;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CimBios.Tools.ModelDebug.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public AsyncRelayCommand ShowDataProviderSelectorCommand { get; }
+
+    private Window? MainWindow 
+    { 
+        get
+        {
+            if (Application.Current?.ApplicationLifetime 
+                is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                return desktop.MainWindow;
+            }
+
+            return null;
+        }
+    }
+
+    public MainWindowViewModel()
+    {
+        ShowDataProviderSelectorCommand = 
+            new AsyncRelayCommand(ShowDataProviderSelectorWindow);
+    }
+
+    private Task ShowDataProviderSelectorWindow()
+    {
+        if (MainWindow == null)
+        {
+            return Task.CompletedTask;
+        }
+
+        var dpsWindow = new DataSelectorWindow()
+        {
+            DataContext = new DataSelectorViewModel()
+        };
+        dpsWindow.ShowDialog(MainWindow);
+
+        return Task.CompletedTask;
+    }
 }
