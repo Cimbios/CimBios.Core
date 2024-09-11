8e357ad9e1541e208df29b6431b60b3905635d7c
belizahrt <belizahrt@gmail.com>
2024-09-12 07:13:51 +1000

    feat: cim objects observer inited

---
+++ src/Tools/CimBios.Tools.ModelDebug/Views/CimObjectsObserverControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CimBios.Tools.ModelDebug.Views;
+
+public partial class CimObjectsObserverControl : UserControl
+{
+    public CimObjectsObserverControl()
+    {
+        InitializeComponent();
+    }
+}
