ccfd19404d10e0f49e484651b27ab578b447bb3e
belizahrt <belizahrt@gmail.com>
2024-09-12 22:30:31 +1000

    feat: CimObjectData model initialized

--- src/Tools/CimBios.Tools.ModelDebug/Models/CimObjects/CimObjectDataTreeModel.cs
+++ src/Tools/CimBios.Tools.ModelDebug/Models/CimObjects/CimObjectDataTreeModel.cs
@@ -1,8 +1,30 @@
 using System;
+using System.Linq;
+using CimBios.Core.CimModel.CimDatatypeLib;
 
 namespace CimBios.Tools.ModelDebug.Models;
 
-public class CimObjectDataTreeModel
+public class CimObjectDataTreeModel : LinkedNodeModel
 {
+    public IModelObject ModelObject { get; set; }
 
+    public string Uuid { get => ModelObject.Uuid; }
+
+    public string Name 
+    {
+        get
+        {
+            if (ModelObject.ObjectData.Attributes.Contains("name"))
+            {
+                return ModelObject.ObjectData.GetAttribute<string>("name");
+            }
+
+            return "noname";
+        }
+    }
+
+    public CimObjectDataTreeModel(IModelObject modelObject) : base()
+    {
+        ModelObject = modelObject;
+    }
 }
