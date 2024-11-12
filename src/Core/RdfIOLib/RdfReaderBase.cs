cdaee668561641462fc3c9f84006d23d50fd325b
belizahrt <belizahrt@gmail.com>
2024-11-12 21:40:51 +1000

    refactor: RdfReaderBase + RdfNamespacesContatinerBase concept -namespaces logic and rdf reader logic generalized to abstract classes

---
+++ src/Core/RdfIOLib/RdfReaderBase.cs
@@ -0,0 +1,39 @@
+namespace CimBios.Core.RdfIOLib;
+
+public abstract class RdfReaderBase : RdfNamespacesContainerBase
+{
+    protected RdfReaderBase() { }
+
+    /// <summary>
+    /// Parse string rdf/xml content.
+    /// </summary>
+    /// <param name="content">String rdf/xml content.</param>
+    public abstract void Parse(string content);
+
+    /// <summary>
+    /// Load rdf/xml content from TextReader.
+    /// </summary>
+    public abstract void Load(TextReader textReader);
+
+    /// <summary>
+    /// Close reader.
+    /// </summary>
+    public abstract void Close();
+
+    /// <summary>
+    /// Resets current reading element to first.
+    /// </summary>
+    public abstract void Reset();
+
+    /// <summary>
+    /// Read RDF content of next element.
+    /// </summary>
+    /// <returns>RDF node of last read element.</returns>
+    public abstract RdfNode? ReadNext();
+
+    // <summary>
+    /// Read all elements in document.
+    /// </summary>
+    /// <returns>Enumerable of RDF nodes</returns>
+    public abstract IEnumerable<RdfNode> ReadAll();
+}
