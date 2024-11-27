425cfdc40d74516ecc2ebdd2c01a73cafdafaeda
belizahrt <53006934+belizahrt@users.noreply.github.com>
2024-11-27 11:26:44 +1000

    Delete src/Core/CimModel/Schema/AutoSchema/CimAutoSchemaFactory.cs

--- src/Core/CimModel/Schema/AutoSchema/CimAutoSchemaFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace CimBios.Core.CimModel.Schema.AutoSchema;
-
-public class CimAutoSchemaFactory : ICimSchemaFactory
-{
-    public ICimSchema CreateSchema()
-    {
-        return new CimSchema(new CimAutoSchemaSerializer());
-    }
-}
