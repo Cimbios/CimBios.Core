9bfc51d2cfafac43861a4531498896184b327979
belizahrt <53006934+belizahrt@users.noreply.github.com>
2024-11-27 16:38:32 +1000

    improvment: Custom types format handling added

--- src/Core/RdfIOLib/RdfWriterBase.cs
+++ src/Core/RdfIOLib/RdfWriterBase.cs
@@ -14,4 +14,19 @@ public abstract class RdfWriterBase : RdfNamespacesContainerBase
     /// <returns>Serialized model XDocument</returns>
     public abstract XDocument Write(IEnumerable<RdfNode> rdfNodes, 
         bool excludeBase = true);
+
+    /// <summary>
+    /// Re-format literal values for specific types.
+    /// </summary>
+    /// <param name="value">Object literal value.</param>
+    /// <returns>Re-formatted value.</returns>
+    protected static object FormatLiteralValue(object value)
+    {
+        if (value is DateTime dateTimeValue)
+        {
+            return dateTimeValue.ToUniversalTime();
+        }
+
+        return value;
+    }
 }
