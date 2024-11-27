814290a85e4915afbf33f36d76888ae3e0da14f2
belizahrt <53006934+belizahrt@users.noreply.github.com>
2024-11-27 11:25:31 +1000

    Delete src/Core/CimModel/Schema/RdfSchema/CimRdfSchemaFactory.cs

--- src/Core/CimModel/Schema/RdfSchema/CimRdfSchemaFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace CimBios.Core.CimModel.Schema.RdfSchema;
-
-public class CimRdfSchemaFactory : ICimSchemaFactory
-{
-    public ICimSchema CreateSchema()
-    {
-        return new CimSchema(new CimRdfSchemaSerializer());
-    }
-}
