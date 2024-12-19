88bd5a67351bdde0d9255a8d9fc6a7dd22245436
belizahrt <53006934+belizahrt@users.noreply.github.com>
2024-12-19 11:18:30 +1000

    Update StdCimDatatypes.cs

--- src/Core/CimModel/CimDatatypeLib/StdCimDatatypes.cs
+++ src/Core/CimModel/CimDatatypeLib/StdCimDatatypes.cs
@@ -1,30 +1,23 @@
 namespace CimBios.Core.CimModel.CimDatatypeLib;
 
-public interface IFullModel : IModelObject
+public interface IFullModel
 {
-    public string Created { get; set; }
-    public string Version { get; set; }
+    public string? Created { get; set; }
+    public string? Version { get; set; }
 }
 
 [CimClass("http://iec.ch/TC57/61970-552/ModelDescription/1#FullModel")]
-public class FullModel : IFullModel
+public class FullModel(DataFacade objectData) 
+    : ModelObject(objectData), IFullModel
 {
-    public string Uuid { get => ObjectData.Uuid; }
-    public string Created 
+    public string? Created 
     { 
         get => ObjectData.GetAttribute<string>("Model.created"); 
         set => ObjectData.SetAttribute("Model.created", value); 
     }
-    public string Version
+    public string? Version
     { 
         get => ObjectData.GetAttribute<string>("Model.version"); 
         set => ObjectData.SetAttribute("Model.version", value);
-    }
-
-    public IDataFacade ObjectData { get; }
-
-    public FullModel(DataFacade objectData)
-    {
-        ObjectData = objectData;
     }
 }
