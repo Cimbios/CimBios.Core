92c2bf44ec6f8a5964c462fa73a651d1109e8060
belizahrt <53006934+belizahrt@users.noreply.github.com>
2024-12-20 10:03:33 +1000

    feat: Create EventUtils.cs and custom events moved [web]

---
+++ src/Core/CimModel/CimDatatypeLib/EventUtils.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using CimBios.Core.CimModel.Schema;
+
+namespace CimBios.Core.CimModel.CimDatatypeLib;
+
+/// <summary>
+/// Can cancel functionality for PropertyChangingEventArgs.
+/// </summary>
+public class CanCancelPropertyChangingEventArgs : PropertyChangingEventArgs
+{
+    public CanCancelPropertyChangingEventArgs(
+        ICimMetaProperty metaProperty, bool cancel)
+        : base(metaProperty?.BaseUri.AbsoluteUri)
+    {
+        Cancel = cancel;
+    }
+
+    /// <summary>
+    /// Cancel property changing flag.
+    /// </summary>
+    public virtual bool Cancel { get; set; }
+}
+
+/// <summary>
+/// ICimMetaProperty based implementation of PropertyChangedEventArgs.
+/// </summary>
+public class CimMetaPropertyChangedEventArgs : PropertyChangedEventArgs
+{
+    public CimMetaPropertyChangedEventArgs(ICimMetaProperty metaProperty) 
+        : base(metaProperty.BaseUri.AbsoluteUri)
+    {
+    }
+}
