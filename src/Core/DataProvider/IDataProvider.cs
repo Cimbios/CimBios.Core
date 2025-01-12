6fdfcc2bfe9744947d4b2d9ba15087024de6a934
belizahrt <belizahrt@gmail.com>
2025-01-12 22:33:49 +1000

    refactor: No caching rdf xml reading implemented - XmlReader based RdfXmlReader instead XDocument (memory optimization) - Streams based IDataProvider re-implemented - minor refactor improvments

--- src/Core/DataProvider/IDataProvider.cs
+++ src/Core/DataProvider/IDataProvider.cs
@@ -2,7 +2,9 @@ namespace CimBios.Core.DataProvider;
 
 public interface IDataProvider
 {
-    public Uri Source { get; set; }
+    public Uri Source { get; }
+    public Stream DataStream { get; }
+    public System.Type Datatype { get; }
 
     public abstract object Get();
     public abstract void Push(object data);
--- src/Core/RdfIOLib/RdfXml/RdfXmlReader.cs
+++ src/Core/RdfIOLib/RdfXml/RdfXmlReader.cs
@@ -1,4 +1,6 @@
-using System.Xml.Linq;
+using System.Xml;
+using System.Text;
+using System.Collections.ObjectModel;
 
 namespace CimBios.Core.RdfIOLib;
 
@@ -8,16 +10,18 @@ namespace CimBios.Core.RdfIOLib;
 /// </summary>
 public sealed class RdfXmlReader : RdfReaderBase
 {
-    /// <summary>
-    /// Root RDF node.
-    /// </summary>
-    private XElement? _RdfElement { get; set; }
+    private XmlReader _XmlReader
+    {
+        get
+        {
+            if (_xmlReader == null)
+            {
+                throw new InvalidOperationException("XmlReader has not been initialized!");
+            }
 
-    /// <summary>
-    /// Stack of read waiting elements.
-    /// </summary>
-    private Stack<XElement> _ReadElementsStack { get; set; }
-        = new Stack<XElement>();
+            return _xmlReader;
+        }
+    }
 
     /// <summary>
     /// Default constructor.
@@ -33,174 +37,278 @@ public sealed class RdfXmlReader : RdfReaderBase
         AddNamespace("base", baseNamespace);
     }
 
-    public override void Parse(string content)
+    public override void Parse(string content, 
+        Encoding? encoding = null)
     {
-        XDocument xDoc = XDocument.Parse(content);
-        Load(xDoc);
+        encoding ??= Encoding.Default;
+
+        var stringBytes = encoding.GetBytes(content);
+        var stringStream = new MemoryStream(stringBytes);
+        var streamReader = new StreamReader(stringStream);
+        Load(streamReader);
     }
 
     public override void Load(TextReader textReader)
     {
-        XDocument xDoc = XDocument.Load(textReader);
-        Load(xDoc);
+        var xmlReader = XmlReader.Create(textReader,
+            new XmlReaderSettings()
+            {
+                IgnoreWhitespace = true,
+                IgnoreComments = true,
+            }
+        );
+
+        Load(xmlReader);
     }
 
     /// <summary>
     /// Load rdf/xml content from XDocument.
     /// </summary>
-    public void Load(XDocument xDoc)
+    public void Load(XmlReader xmlReader)
     {
-        ReadRdfRootNode(xDoc);
-        Reset();
+        _xmlReader = xmlReader;
+
+        if (_XmlReader.ReadState != ReadState.Initial
+            && _XmlReader.ReadState != ReadState.Interactive)
+        {
+            throw new Exception("XmlReader has not been initialized!");
+        }
+
+        if (ReadRdfRootNode() == false)
+        {
+            throw new Exception("Xml document does not contains rdf:RDF root node!");
+        }
     }
 
     public override void Close()
     {
-        _RdfElement = null;
         ClearNamespaces();
-        _ReadElementsStack.Clear();
+        _xmlReader?.Close();
     }
-
-    public override RdfNode? ReadNext()
-    {
-        if (_ReadElementsStack.Count() == 0)
-        {
-            return null;
-        }
-
-        XElement content = _ReadElementsStack.Pop();
-
-        string subjectId = GetXElementIdentifier(content, out bool isAuto);
-        Uri subject = NameToUri(subjectId);
-
-        Uri typeIdentifier = new Uri(content.Name.Namespace.NamespaceName
-                + content.Name.LocalName);
-
-        var rdfNode = new RdfNode(subject, typeIdentifier, isAuto);
-
-        ///var triples = new List<RdfTriple>(content.Elements().Count());
-        foreach (var child in content.Elements())
-        {
-            Uri predicate = new Uri(child.Name.Namespace.NamespaceName
-                + child.Name.LocalName);
-
-            // Blank node
-            if (child.HasElements == false)
-            {
-                XAttribute? resource = child
-                    .Attribute(rdf + "resource");
-
-                object? @object;
-                // Blank node
-                if (resource != null)
-                {
-                    @object = NameToUri(resource.Value);
-                }
-                // Literal node
-                else
-                {
-                    @object = child.Value;
-                }
-
-                rdfNode.NewTriple(predicate, @object);
-            }
-            // Element node
-            else if (child.HasElements == true)
-            {
-                foreach (var el in child.Elements())
-                {
-                    _ReadElementsStack.Push(el);
-                    var subObject = ReadNext();
-                    if (subObject == null)
-                    {
-                        continue;
-                    }
-
-                    rdfNode.NewTriple(predicate, subObject);
-                }
-            }
-            else
-            {
-                throw new Exception("Cannot parse rdf node");
-            }
-        }
-
-        return rdfNode;
-    }
-
+    
     public override IEnumerable<RdfNode> ReadAll()
     {
-        if (_RdfElement == null)
-        {
-            throw new Exception("No rdf node");
-        }
-
-        Reset();
-
         RdfNode? rdfNode;
         while ((rdfNode = ReadNext()) != null)
         {
             yield return rdfNode;
         }
-
-        Reset();
     }
-    
-    public override void Reset()
+
+    public override RdfNode? ReadNext()
     {
-        if (_RdfElement == null)
+        if (CanReadNext() == false)
         {
-            throw new Exception("No rdf node");
+            return null;
         }
 
-        _ReadElementsStack.Clear();
-        _RdfElement.Elements().Reverse().ToList()
-            .ForEach(el => _ReadElementsStack.Push(el));
+        if (SkipToNextElement() == false)
+        {
+            return null;
+        }
+
+        var nodesStack = new Stack<RdfNode>();
+        var subtreeReader = _XmlReader.ReadSubtree();
+        RdfNode? rdfNode = null;
+        bool blockReading = false;
+
+        do
+        {
+            if (blockReading == false 
+                && subtreeReader.Read() == false)
+            {
+                break;
+            }
+            blockReading = false;
+
+            if (subtreeReader.NodeType == XmlNodeType.EndElement
+                && (subtreeReader.Depth & 1) == 0)
+            {
+                _XmlReader.ReadEndElement();
+                rdfNode = nodesStack.Pop();
+
+                if (nodesStack.Count != 0)
+                {
+                    nodesStack.Peek().NewTriple(
+                        NameToUri(_XmlReader.Name), 
+                        rdfNode);
+                }
+
+                continue;
+            }
+
+            if (subtreeReader.NodeType != XmlNodeType.Element)
+            {
+                continue;
+            }
+
+            int initialDepth = subtreeReader.Depth;
+
+            var nodeInfo = ReadNodeHeader();
+            // Depth parity is node-predicate arc marker.
+            if ((initialDepth & 1) == 0)
+            {
+                var nodeIRI = NameToUri(nodeInfo.Identifier);
+                var typeIRI = NameToUri(nodeInfo.TypeIdentifier);
+                var nextNode = new RdfNode(nodeIRI, typeIRI, nodeInfo.IsAuto);
+                
+                nodesStack.Push(nextNode);
+            }
+            else
+            {
+                if (nodeInfo.AttributesMap.ContainsKey(rdf + "resource"))
+                {
+                    nodesStack.Peek().NewTriple(
+                        NameToUri(nodeInfo.TypeIdentifier), 
+                        NameToUri(nodeInfo.Identifier));
+                }
+                else if (subtreeReader.Read() 
+                    && subtreeReader.NodeType == XmlNodeType.Text)
+                {
+                    nodesStack.Peek().NewTriple(
+                        NameToUri(nodeInfo.TypeIdentifier), 
+                        subtreeReader.Value);
+                }
+                else if (subtreeReader.NodeType == XmlNodeType.Element)
+                {
+                    blockReading = true;
+                }
+            }
+        }
+        while (nodesStack.Count != 0);
+
+        return rdfNode;
+    }
+
+    /// <summary>
+    /// Skip while Element not found or eof
+    /// </summary>
+    /// <returns>True if next element reached.</returns>
+    private bool SkipToNextElement()
+    {
+        while (_XmlReader.NodeType != XmlNodeType.Element)
+        {   
+            if (_XmlReader.Read() == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Get element identifier from rdf:about/id. 
+    /// Makes auto identifier in case of unidetified element.
+    /// </summary>
+    /// <returns>dcd</returns>
+    private RdfXmlNodeInfo ReadNodeHeader()
+    {
+        var info = new RdfXmlNodeInfo()
+        {
+            TypeIdentifier = _XmlReader.NamespaceURI + _XmlReader.LocalName,
+            IsEmpty = _XmlReader.IsEmptyElement,
+            IsAuto = false
+        };
+
+        var attributes = ReadNodeAttributes();
+        info.AttributesMap = attributes.AsReadOnly();
+
+        if (attributes.TryGetValue(rdf + "about", out var aboutName))
+        {
+            info.Identifier = aboutName;
+        }
+        else if (attributes.TryGetValue(rdf + "ID", out var IDName))
+        {
+            info.Identifier = IDName;
+        }
+        else if (attributes.TryGetValue(rdf + "resource", out var resourceName))
+        {
+            info.Identifier = resourceName;
+        }
+        else
+        {
+            info.IsAuto = true;
+            info.Identifier = $"#_auto{_XmlReader.GetHashCode()}{info.GetHashCode()}";
+        }
+
+        return info;
+    }
+
+    /// <summary>
+    /// Get reading ability status.
+    /// </summary>
+    /// <returns>True if reading is available.</returns>
+    private bool CanReadNext()
+    {
+        return _xmlReader != null
+            && _xmlReader.ReadState != ReadState.Closed
+            && _xmlReader.ReadState != ReadState.Error
+            && _xmlReader.ReadState != ReadState.EndOfFile;
+    }
+
+    /// <summary>
+    /// Read attributes from element node.
+    /// </summary>
+    /// <returns>Pairs (attr uri, value) set.</returns>
+    private Dictionary<string, string> ReadNodeAttributes()
+    {
+        var result = new Dictionary<string, string>();
+
+        if (_XmlReader.HasAttributes == false)
+        {
+            return result;
+        }
+
+        while (_XmlReader.MoveToNextAttribute())
+        {
+            var uri = _XmlReader.NamespaceURI + _XmlReader.LocalName;
+            result.Add(uri, _XmlReader.Value);
+        }
+
+        return result;
     }
 
     /// <summary>
     /// Get rdf:RDF root node.
     /// </summary>
-    /// <param name="content">Linq Xml document.</param>
-    private void ReadRdfRootNode(XDocument content)
+    private bool ReadRdfRootNode()
     {
-        XElement? rdfNode = content.Element(rdf + "RDF");
-        if (rdfNode == null)
+        if (_XmlReader.ReadToFollowing("RDF", rdf)
+            && _XmlReader.NodeType == XmlNodeType.Element)
         {
-            throw new Exception("No RDF Node");
+            ParseXmlns();
+            return true;
         }
 
-        _RdfElement = rdfNode;
-        rdfNode.Elements().Reverse().ToList()
-            .ForEach(el => _ReadElementsStack.Push(el));
-
-        ParseXmlns(rdfNode);
+        return false;
     }
 
     /// <summary>
-    /// Get element identifier from rdf:about/id. Makes auto identifier in case of undentified element.
+    /// Parse all root namespaces.
     /// </summary>
-    /// <param name="element">Xml identified element.</param>
-    /// <param name="isAuto">Set is auto identifier assigned.</param>
-    private string GetXElementIdentifier(XElement element, out bool isAuto)
+    private void ParseXmlns()
     {
-        isAuto = false;
-        XAttribute? about = element.Attribute(rdf + "about");
-        if (about == null)
+        while (_XmlReader.MoveToNextAttribute())
         {
-            XAttribute? ID = element.Attribute(rdf + "ID");
-
-            if (ID == null)
+            if (_XmlReader.NamespaceURI != xmlns
+                && _XmlReader.Name != "xml:base")
             {
-                isAuto = true;
-                return $"#_auto{element.GetHashCode()}";
+                continue;
             }
 
-            return ID.Value;
+            AddNamespace(_XmlReader.LocalName, new Uri(_XmlReader.Value));
         }
-        else
-        {
-            return about.Value;
-        }
+    }
+
+    private XmlReader? _xmlReader = null;
+
+    private struct RdfXmlNodeInfo
+    {
+        public string Identifier { get; set; }
+        public bool IsAuto { get; set; }
+        public string TypeIdentifier { get; set; }
+        public bool IsEmpty { get; set; }
+        public ReadOnlyDictionary<string, string> AttributesMap { get; set; }
     }
 }
