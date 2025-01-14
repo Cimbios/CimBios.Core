5f2174c5c688e58efa3b27b6ac5609988c4b2c19
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-01-14 15:40:14 +1000

    feat: Create TemplateReader.cs [web]
    Code template file parser. Providex syntax blocks

---
+++ src/Tools/CimBios.Tools.CimTypeLibBuilder/TemplateReader/TemplateReader.cs
@@ -0,0 +1,112 @@
+namespace CimBios.Tools.CimTypeLibBuilder.TemplateReader;
+
+internal static class TemplateReader
+{
+    public static string RootBlock = "root";
+
+    public static IEnumerable<CodeBlockSyntax> ReadTemplate(string templatePath)
+    {
+        var reader = new StreamReader(templatePath);
+        
+        string cursor = RootBlock;
+        var rootCodeBlock = new CodeBlockSyntax(RootBlock);
+
+        var blocksCache = new Dictionary<string, CodeBlockSyntax>
+        {
+            { rootCodeBlock.ClassName, rootCodeBlock }
+        };
+
+        int lineNumber = 0;
+        do
+        {
+            ++lineNumber;
+            var line = reader.ReadLine();
+
+            if (line == null || line == string.Empty)
+            {
+                continue;
+            }
+            else if (line.Trim() == "~")
+            {
+                blocksCache[cursor].Text += "\n";
+                continue;
+            }
+            else if (line.Trim().Length > 3 && line.Trim()[..2] == "@:" 
+                && line.Trim().Last() == ':')
+            {
+                var className = line.Trim()[2..(line.Trim().Length-1)];
+
+                if (cursor == RootBlock)
+                {
+                    var definition = new CodeBlockSyntax(className);
+                    blocksCache.Add(definition.ClassName, definition);
+                    cursor = className;
+                }
+                else if (className == cursor)
+                {
+                    blocksCache[cursor].Text =  blocksCache[cursor].Text[..^1];
+                        
+                    cursor = RootBlock;
+                }
+                else
+                {
+                    throw new Exception("Unexpected class definition in line " + lineNumber);
+                }
+            }
+            else
+            {
+                blocksCache[cursor].Text += line + "\n";
+                var variables = ParseSpecs(line, '$', lineNumber);
+                foreach (var variable in variables.Distinct())
+                {
+                    if (blocksCache[cursor].Variables.Contains(variable))
+                    {
+                        continue;
+                    }
+
+                    blocksCache[cursor].Variables.Add(variable);
+                }
+
+                var refs = ParseSpecs(line, '&', lineNumber);
+                foreach (var refName in refs.Distinct())
+                {
+                    if (blocksCache[cursor].References.Contains(refName))
+                    {
+                        continue;
+                    }
+
+                    if (blocksCache.ContainsKey(refName) == false)
+                    {
+                        throw new Exception("Undefined class reference in line " + lineNumber);
+                    }
+
+                    blocksCache[cursor].References.Add(refName);
+                }
+            }
+        }
+        while (reader.EndOfStream == false);
+        reader.Close();
+
+        return blocksCache.Values;
+    }
+
+    private static string[] ParseSpecs(string line, char spec, int lineNumber)
+    {
+        var variables = new List<string>();
+        int fid = 0;
+
+        while ((fid = line.IndexOf(spec, fid)) != -1)
+        {
+            var closeId = line.IndexOf(":", fid+2);
+            if (closeId == -1)
+            {
+                throw new Exception("Unexpected end of the line while variable definition in line" + lineNumber);
+            }
+
+            variables.Add(line.Substring(fid+2, closeId-fid-2));
+            fid = closeId;
+        }
+
+        return variables.ToArray();
+    }
+}
