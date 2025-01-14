c1479a5ed20796dce88cf884786c34a7615f73ed
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-01-14 15:39:23 +1000

    feat: Create CodeBlockSyntax.cs
    Syntax code block definitions and building infos

---
+++ src/Tools/CimBios.Tools.CimTypeLibBuilder/TemplateReader/CodeBlockSyntax.cs
@@ -0,0 +1,10 @@
+namespace CimBios.Tools.CimTypeLibBuilder.TemplateReader;
+
+internal class CodeBlockSyntax(string className)
+{
+
+    public string ClassName { get; set; } = className;
+    public string Text { get; set; } = string.Empty;
+    public HashSet<string> Variables { get; } = [];
+    public HashSet<string> References { get; } = [];
+}
