5d2622ac617af82d5b73bbe4cd345b19b6d797d9
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-01-14 15:38:21 +1000

    feat: console program implemented [web]

---
+++ src/Tools/CimBios.Tools.CimTypeLibBuilder/Program.cs
@@ -0,0 +1,60 @@
+using System.CommandLine;
+using System.CommandLine.Builder;
+using CimBios.Core.CimModel.Schema.RdfSchema;
+using CimBios.Tools.CimTypeLibBuilder.CodeBuilder;
+using CimBios.Tools.CimTypeLibBuilder.TemplateReader;
+
+// ----------------------------------------------------------------------------
+
+#region Command line args reading
+var schemaPathArgument = new Argument<string>
+    ("schema-file", "The input schema file to process");
+var templatePathArgument = new Argument<string>
+    ("template-file", "The input code template file to process");
+var namespaceOption = new Option<string>
+    ("-namespace", "The input schema file to process") { IsRequired = true };
+var serializerOption = new Option<SerializerTypeInfo>("-serializer", 
+        () => SerializerTypeInfo.RDFS, "The input schema serializer type");
+
+var rootCommand = new RootCommand
+{
+    schemaPathArgument,
+    templatePathArgument,
+    namespaceOption,
+    serializerOption,
+};
+
+var rootCommandParser = new CommandLineBuilder(rootCommand)
+    .UseDefaults()
+    .Build();
+
+var parseResult = rootCommandParser.Parse(args);
+#endregion Command line args reading
+
+#region Schema reading
+var schemaPath = parseResult.GetValueForArgument(schemaPathArgument);
+var cimSchema = new CimRdfSchemaXmlFactory().CreateSchema();
+cimSchema.Load(new StreamReader(schemaPath));
+#endregion Schema reading
+
+#region Template reading
+var templatePath = parseResult.GetValueForArgument(templatePathArgument); 
+var templateCodeBlocks = TemplateReader.ReadTemplate(templatePath);
+#endregion Template reading
+
+#region Typelib compilation
+var namespaceOptionValue = parseResult.GetValueForOption(namespaceOption); 
+var codePath = templatePath.Replace(Path.GetExtension(templatePath), ".blc");
+var codeBuilder = new CodeBuilder(cimSchema, templateCodeBlocks.ToArray(), 
+    namespaceOptionValue!);
+codeBuilder.Compile(codePath);
+#endregion Typelib compilation
+
+Console.ReadLine();
+
+// ----------------------------------------------------------------------------
+public enum SerializerTypeInfo
+{
+    Auto,
+    RDFS,
+}
