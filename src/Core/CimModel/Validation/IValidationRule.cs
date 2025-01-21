96a5538e2fcb9972836f280c43b7b68e6845e8a5
Александр Чугуевский <a1998ch@gmail.com>
2025-01-21 19:41:33 +1000

    Created dev_Validation

---
+++ src/Core/CimModel/Validation/IValidationRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CimBios.Core.CimModel.CimDatatypeLib;
+
+namespace CimBios.Core.CimModel.Validation
+{
+    public interface IValidationRule
+    {
+        /// <summary>
+        /// Метод для получения списка результатов проверки объекта CIM
+        /// </summary>
+        /// <returns>Список результатов проверки объекта CIM</returns>
+        IEnumerable<ValidationResult> Execute(IModelObject modelObject);
+    }
+}
---
+++ src/Core/CimModel/Validation/ValidationManager.cs
@@ -0,0 +1,78 @@
+﻿using CimBios.Core.CimModel.CimDatatypeLib;
+using System;
+using System.Reflection;
+
+namespace CimBios.Core.CimModel.Validation
+{
+    public class ValidationManager
+    {
+
+        /// <summary>
+        /// Результаты проверки при чтении фрагмента
+        /// </summary>
+        /// <param name="modelObject">Объект CIM из фрагмента</param>
+        /// <returns>Список результатов проверки</returns>
+        /// <exception cref="ArgumentNullException">ArgumentNullException</exception>
+        public List<(string? Uuid, ValidationResultType ResultType, string? Message)> Validate(
+            IModelObject modelObject)
+        {
+            var validationResults = new List<Validation.ValidationResult>();
+
+            var typeValidationAttributes = new ValidationManager(
+                ).LoadValidAssembly();
+
+            try
+            {
+                validationResults = typeValidationAttributes.Select(
+                    va =>
+                    {
+                        var validInstance = Activator.CreateInstance(
+                            va, modelObject, Schema);
+
+                        if (validInstance == null)
+                            throw new ArgumentNullException();
+
+                        return ((SchemaValidationRuleBase)validInstance).Execute();
+                    }
+                ).First().
+                  ToList();
+            }
+            catch (Exception ex)
+            {
+                return new List<(
+                    string? Uuid,
+                    ValidationResultType ResultType,
+                    string? Message)>()
+            {
+                (modelObject.Uuid,
+                ValidationResultType.fail,
+                ex.Message)
+            };
+            }
+
+            var validMessage = new List<(
+                string? Uuid,
+                ValidationResultType ResultType,
+                string? Message)>();
+
+            foreach (var vr in validationResults)
+            {
+                validMessage.Add(
+                    (vr.ModelObject?.Uuid, vr.ResultType, vr.Message)
+                    );
+            }
+
+            return validMessage;
+        }
+
+        private IEnumerable<Type> LoadValidAssembly()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var validationTypes = assembly.GetTypes()
+                .Where(t => t.IsDefined(typeof(AttributeValidation), true));
+
+            return validationTypes;
+        }
+    }
+}
