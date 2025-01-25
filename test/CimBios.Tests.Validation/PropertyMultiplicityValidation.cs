2e3e9291efe09e2e7858d4d975dd3cb3cbeab74f
belizahrt <belizahrt@gmail.com>
2025-01-25 18:59:04 +1000

    test: Test project initialized

---
+++ test/CimBios.Tests.Validation/PropertyMultiplicityValidation.cs
@@ -0,0 +1,16 @@
+namespace CimBios.Tests.Validation;
+
+public class PropertyMultiplicityValidation
+{
+    [Fact]
+    public void StrictlyOne()
+    {
+
+    }
+
+    [Fact]
+    public void OneN()
+    {
+
+    }
+}
