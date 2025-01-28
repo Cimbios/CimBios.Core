e1dcc57759bc7c4314e519010db715891fc347ae
Александр Чугуевский <a1998ch@gmail.com>
2025-01-28 18:25:14 +1000

    fix : Corrected PropertyMultiplicityValidationRule. feat : Complited AutoTest

--- test/CimBios.Tests.Validation/PropertyMultiplicityValidation.cs
+++ test/CimBios.Tests.Validation/PropertyMultiplicityValidation.cs
@@ -7,14 +7,25 @@ public class PropertyMultiplicityValidation
     [Fact]
     public void StrictlyOne()
     {
+        var isCorrectValid = IsValidMultiplicity(
+            "testReactiveCapabilityCurve");
 
+        Assert.True(isCorrectValid);
     }
 
     [Fact]
     public void OneN()
     {
+        var isCorrectValid = IsValidMultiplicity(
+            "testACLineSeriesSection");
+
+        Assert.True(isCorrectValid);
+    }
+
+    private bool IsValidMultiplicity(string inputData)
+    {
         var cimDocument = CimDocumentLoader.LoadCimDocument(
-            "../../../assets/testACLineSeriesSection.xml",
+            $"../../../assets/{inputData}.xml",
             "../../../../../assets/cimrdfs_schemas/cimbios-cim17-RUCIM.rdfs");
 
         var objects = cimDocument.GetAllObjects().Where(x => x != null);
@@ -24,16 +35,21 @@ public class PropertyMultiplicityValidation
         var rules = manager.GetValidationRules;
 
         var multiplicityRule = rules.Where(
-            r => r.GetType() == 
-            typeof(PropertyMultiplisityValidationRule)
-            ).First();
+            r => r.GetType() ==
+            typeof(PropertyMultiplicityValidationRule)
+        ).First();
 
-        var resultValidation = objects.Select(
-            o => multiplicityRule.Execute(o));
+        var resultValidation = objects.Select(multiplicityRule.Execute);
 
-        var isCorrectValid = resultValidation.All(
-            x => x.All(y => y.ResultType == ValidationResultKind.Pass));
+        var isCorrectValid = resultValidation.Any(
+            x =>
+            {
+                var correctResult = x.Where(
+                    y => y.ResultType == ValidationResultKind.Pass);
+                return correctResult != null;
+            }
+        );
 
-        Assert.True(isCorrectValid);
+        return isCorrectValid;
     }
 }
