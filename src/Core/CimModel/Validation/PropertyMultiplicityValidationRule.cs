9512c1719038320255666f609d0c5a2061e7a840
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-01-30 10:28:39 +1000

    refactor: GetPropertyValueAsObject commoned to base class

--- src/Core/CimModel/Validation/PropertyMultiplicityValidationRule.cs
+++ src/Core/CimModel/Validation/PropertyMultiplicityValidationRule.cs
@@ -40,27 +40,5 @@ namespace CimBios.Core.CimModel.Validation
                     ModelObject = modelObject
                 };
         }
-
-        /// <summary>
-        /// Get any value (attribute or assoc) of model object by meta property.
-        /// </summary>
-        /// <param name="modelObject">Model object instance.</param>
-        /// <param name="property">Meta property.</param>
-        /// <returns>Object value or null if property value does not exist.</returns>
-        private object? GetPropertyValueAsObject(
-            IModelObject modelObject, ICimMetaProperty property)
-        {
-            switch (propertiesRequied.PropertyKind)
-            {
-                case CimMetaPropertyKind.Attribute:
-                    return modelObject.
-                        GetAttribute(propertiesRequied);
-                case CimMetaPropertyKind.Assoc1ToM:
-                    return modelObject.
-                        GetAssoc1ToM(propertiesRequied).FirstOrDefault();
-            }
-            
-            return null;
-        }
     }
 }
