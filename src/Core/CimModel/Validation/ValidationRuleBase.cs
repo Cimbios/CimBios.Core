eb69130a51d4e4829a5d36378b721b4f1d9532ea
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-01-30 10:29:37 +1000

    refactor: GetPropertyValueAsObject commoned to base class

--- src/Core/CimModel/Validation/ValidationRuleBase.cs
+++ src/Core/CimModel/Validation/ValidationRuleBase.cs
@@ -7,20 +7,41 @@ using System.Text;
 using CimBios.Core.CimModel.Schema;
 using System.Threading.Tasks;
 
-namespace CimBios.Core.CimModel.Validation
+namespace CimBios.Core.CimModel.Validation;
+
+public abstract class ValidationRuleBase : IValidationRule
 {
-    public abstract class ValidationRuleBase : IValidationRule
+    /// <inheritdoc/>
+    public abstract IEnumerable<ValidationResult> Execute(
+        IModelObject modelObject);
+
+    /// <summary>
+    /// Конструктор класса ValidationRuleBase
+    /// </summary>
+    protected ValidationRuleBase()
     {
-        /// <inheritdoc/>
-        public abstract IEnumerable<ValidationResult> Execute(
-            IModelObject modelObject);
 
-        /// <summary>
-        /// Конструктор класса ValidationRuleBase
-        /// </summary>
-        protected ValidationRuleBase()
+    }
+
+    /// <summary>
+    /// Get any value (attribute or assoc) of model object by meta property.
+    /// </summary>
+    /// <param name="modelObject">Model object instance.</param>
+    /// <param name="property">Meta property.</param>
+    /// <returns>Object value or null if property value does not exist.</returns>
+    protected object? GetPropertyValueAsObject(
+        IModelObject modelObject, ICimMetaProperty property)
+    {
+        switch (propertiesRequied.PropertyKind)
         {
-
+            case CimMetaPropertyKind.Attribute:
+                return modelObject.
+                    GetAttribute(propertiesRequied);
+            case CimMetaPropertyKind.Assoc1ToM:
+                return modelObject.
+                    GetAssoc1ToM(propertiesRequied).FirstOrDefault();
         }
+        
+        return null;
     }
 }
