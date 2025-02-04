b4afe3e1ab67488af6af5980fb3ac9d9f8381055
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-02-04 19:34:23 +1000

    Update LoadTest.cs

--- test/CimBios.Tests.DifferenceModel/LoadTest.cs
+++ test/CimBios.Tests.DifferenceModel/LoadTest.cs
@@ -1,10 +1,31 @@
+using CimBios.Core.CimModel.CimDatatypeLib;
+using CimBios.Core.CimModel.RdfSerializer;
+using CimBios.Core.CimModel.Schema;
+using CimBios.Core.CimModel.Schema.RdfSchema;
+using CimBios.Core.DifferenceModel;
+
 namespace CimBios.Tests.DifferenceManager;
 
 public class LoadTest
 {
     [Fact]
-    public void Test1()
+    public void LoadAndGetDifferenceModelObject()
     {
+        var schema = LoadCimSchema("../../../assets/Iec61970-552-Headers-rdfs.xml");
+        var rdfSerializer = new RdfXmlSerializer(schema, new CimDatatypeLib());
+        var cimDifferenceModel = new CimDifferenceModel(rdfSerializer);
 
+        cimDifferenceModel.Load(new StreamReader("../../../assets/test_diff.xml"));
+    }
+
+    private static ICimSchema LoadCimSchema(string path, 
+        ICimSchemaFactory? factory = null)
+    {
+        factory ??= new CimRdfSchemaXmlFactory();
+        var cimSchema = factory.CreateSchema();
+
+        cimSchema.Load(new StreamReader(path));
+
+        return cimSchema;
     }
 }
