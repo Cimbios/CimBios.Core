7f08ed0bc5619cc350c1bcf43a4f584bedd37a90
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-02-04 19:32:44 +1000

    feat: few definitions

--- src/Core/CimDifferenceModel/IDifferenceModel.cs
+++ src/Core/CimDifferenceModel/IDifferenceModel.cs
@@ -1,10 +1,25 @@
-namespace CimBios.Core.CimDifferenceModel;
+using System.Text;
+using CimBios.Core.CimModel.CimDataModel;
+
+namespace CimBios.Core.DifferenceModel;
 
 /// <summary>
 /// 
 /// </summary>
-public interface IDifferenceModel
+public interface ICimDifferenceModel
 {
+    public void Load(StreamReader streamReader);
+    public void Load(string path);
+    public void Parse(string content, Encoding? encoding = null);
+
+    public void Save(StreamWriter streamWriter);
+    public void Save(string path);
+
+    public void ExtractFromDataModel(ICimDataModel cimDataModel);
+    public void InvalidateDataWithModel(ICimDataModel cimDataModel);
+
+    public void ResetAll();
+
     // forward
     // reference
     // compare
