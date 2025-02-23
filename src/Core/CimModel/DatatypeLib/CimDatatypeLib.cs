67670ed795a29d219cd4d5680457f300da882816
belizahrt <belizahrt@gmail.com>
2025-02-23 12:36:55 +1000

    refactor: directories-files restructure

--- src/Core/CimModel/DatatypeLib/CimDatatypeLib.cs
+++ src/Core/CimModel/DatatypeLib/CimDatatypeLib.cs
@@ -1,62 +1,8 @@
 using CimBios.Core.CimModel.Schema;
-using CimBios.Core.RdfIOLib;
 using CimBios.Utils.ClassTraits;
 using System.Reflection;
 
 namespace CimBios.Core.CimModel.CimDatatypeLib;
-
-/// <summary>
-/// Structure interface for datatype lib.
-/// </summary>
-public interface ICimDatatypeLib : ICanLog
-{
-    /// <summary>
-    /// Dictionary Uri to Type of IModelObject concrete classes.
-    /// </summary>
-    public IReadOnlyDictionary<ICimMetaClass, System.Type> RegisteredTypes { get; }
-
-    /// <summary>
-    /// Load assembly by file path.
-    /// </summary>
-    /// <param name="typesAssemblyPath">Path to assembly .dll file.</param>
-    /// <param name="reset">Clear current assemblies collection.</param>
-    public void LoadAssembly(string typesAssemblyPath, bool reset = true);
-
-    /// <summary>
-    /// Load assembly by file path.
-    /// </summary>
-    /// <param name="typesAssembly">Assembly object.</param>
-    /// <param name="reset">Clear current assemblies collection.</param>
-    public void LoadAssembly(Assembly typesAssembly, bool reset = true);
-
-    /// <summary>
-    /// Register new type in datatype library.
-    /// </summary>
-    /// <param name="type">Type for register.</param>
-    public void RegisterType(System.Type type);
-
-    /// <summary>
-    /// Create instance of schema meta class.
-    /// </summary>
-    /// <param name="modelObjectFactory">Model object factory.</param>
-    /// <param name="oid">OID of creating instance.</param>
-    /// <param name="metaClass">Cim schema meta class.</param>
-    /// <param name="isAuto">Is auto class attribute.</param>
-    /// <returns>IModelObject instance of meta type.</returns>
-    public IModelObject? CreateInstance(IModelObjectFactory modelObjectFactory,
-        string oid, ICimMetaClass metaClass, bool isAuto);
-
-    /// <summary>
-    /// Create instance of schema meta class.
-    /// </summary>
-    /// <typeparam name="T">IModelObject CIM lib type.</typeparam>
-    /// <param name="oid">OID of creating instance.</param>
-    /// <param name="metaClass">Cim schema meta class.</param>
-    /// <param name="isAuto">Is auto class attribute.</param>
-    /// <returns>IModelObject instance of meta type.</returns>
-    public T? CreateInstance<T>(string oid, bool isAuto) 
-        where T : class, IModelObject;
-}
 
 /// <summary>
 /// Concrete model objects types library class.
