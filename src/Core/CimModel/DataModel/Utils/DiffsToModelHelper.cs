efce214c445cfa989aa1bb1cddc65c477f8a37c0
belizahrt <belizahrt@gmail.com>
2025-03-02 18:35:20 +1000

    feat: helpers classes

---
+++ src/Core/CimModel/DataModel/Utils/DiffsToModelHelper.cs
@@ -0,0 +1,60 @@
+using CimBios.Core.CimModel.CimDataModel;
+using CimBios.Core.CimModel.CimDatatypeLib;
+using CimBios.Core.CimModel.Schema;
+
+namespace CimBios.Core.CimModel.DataModel.Utils;
+
+internal static class DiffsToModelHelper
+{
+    internal static void ApplyTo(ICimDataModel model, 
+        IEnumerable<IDifferenceObject> differences)
+    {
+        differences.AsParallel().ForAll(diff =>
+        {
+            if (diff is AdditionDifferenceObject)
+            {
+                var schemaMetaClass = model.Schema
+                    .TryGetResource<ICimMetaClass>(diff.MetaClass.BaseUri);
+                
+                if (schemaMetaClass == null)
+                {
+                    return;
+                }
+                
+                var addedObject = model.CreateObject(diff.OID, schemaMetaClass);
+                if (schemaMetaClass == null)
+                {
+                    /// exception?
+                    return;
+                }
+
+                addedObject.CopyPropertiesFrom(diff.ModifiedObject, true);
+                ResolveReferencesInModelObject(model, addedObject);
+
+            }
+        });
+    }
+
+    private static void ResolveReferencesInModelObject(ICimDataModel model,
+        IModelObject modelObject)
+    {
+        foreach (var metaProperty in modelObject.MetaClass
+            .AllProperties
+            .Where(p => p.PropertyKind == CimMetaPropertyKind.Assoc1To1))
+        {
+            var assocObj = modelObject.GetAssoc1To1<IModelObject>(metaProperty);
+            if (assocObj is not ModelObjectUnresolvedReference)
+            {
+                continue;
+            }
+
+            var referenceObject = model.GetObject(assocObj.OID);
+            if (referenceObject == null)
+            {
+                continue;
+            }
+
+            modelObject.SetAssoc1To1(metaProperty, referenceObject);
+        }
+    }
+}
