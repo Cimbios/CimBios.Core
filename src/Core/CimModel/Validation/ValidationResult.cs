540bfe295df3f280838212826bbb3644c635a476
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-04-04 16:52:04 +1000

    feat: validation result types implemented
    - simple ModelValidationResult (4 full model)
    - model object+ property? ModelObjectValidationResult
    - mock PassValidationResult

--- src/Core/CimModel/Validation/ValidationResult.cs
+++ src/Core/CimModel/Validation/ValidationResult.cs
@@ -1,34 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CimBios.Core.CimModel.CimDatatypeLib;
+using CimBios.Core.CimModel.Schema;
 
-namespace CimBios.Core.CimModel.Validation
+namespace CimBios.Core.CimModel.Validation;
+
+public interface IValidationResult
 {
-    public class ValidationResult
+    /// <summary>
+    /// Тип результата валидации
+    /// </summary>
+    public ValidationResultKind ResultType { get; }
+
+    /// <summary>
+    /// Сообщение после проверки
+    /// </summary>
+    public string Message { get; }       
+}
+
+/// <summary>
+/// Just pass-mock validation result class.
+/// </summary>
+public class PassValidationResult : IValidationResult
+{
+    public PassValidationResult()
+    {}
+
+    public PassValidationResult(string message)
     {
-        /// <summary>
-        /// Тип результата валидации
-        /// </summary>
-        public ValidationResultKind ResultType;
-
-        /// <summary>
-        /// Сообщение после проверки
-        /// </summary>
-        public string Message = string.Empty;
-
-        /// <summary>
-        /// Объект CIM
-        /// </summary>
-        public IModelObject? ModelObject;
+        Message = message;
     }
 
-    public enum ValidationResultKind
+    public ValidationResultKind ResultType => ValidationResultKind.Pass;
+    
+    public string Message { get; } = string.Empty;
+}
+
+public class ModelValidationResult (ValidationResultKind resultType, 
+        string message)
+    : IValidationResult
+{
+    /// <summary>
+    /// Тип результата валидации
+    /// </summary>
+    public ValidationResultKind ResultType { get; } = resultType;
+
+    /// <summary>
+    /// Сообщение после проверки
+    /// </summary>
+    public string Message { get; } = message;
+}
+
+public class ModelObjectValidationResult(ValidationResultKind resultType,
+        string message, IReadOnlyModelObject modelObject, 
+        ICimMetaProperty? metaProperty = null) 
+    : ModelValidationResult(resultType, message) 
+{
+    /// <summary>
+    /// Объект CIM
+    /// </summary>
+    public IReadOnlyModelObject? ModelObject
     {
-        Pass,
-        Fail,
-        Warning
+        get
+        {
+            if (_ModelObjectRef.TryGetTarget(out var mo))
+            {
+                 return mo;
+            }
+
+            return null;
+        }
     }
+
+    /// <summary>
+    /// Свойство объекта
+    /// </summary>
+    public ICimMetaProperty? Property { get; } = metaProperty;
+
+    protected WeakReference<IReadOnlyModelObject> _ModelObjectRef 
+        = new(modelObject);
+}
+
+public enum ValidationResultKind
+{
+    Pass,
+    Fail,
+    Warning
 }
