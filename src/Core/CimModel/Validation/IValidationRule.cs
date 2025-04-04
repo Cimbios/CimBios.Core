3359416cfb51a1a8323e6e2c3362ce01a69c9086
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-04-04 16:49:07 +1000

    feat: NeedExecute state implemented

--- src/Core/CimModel/Validation/IValidationRule.cs
+++ src/Core/CimModel/Validation/IValidationRule.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CimBios.Core.CimModel.CimDatatypeLib;
 
-namespace CimBios.Core.CimModel.Validation
+namespace CimBios.Core.CimModel.Validation;
+
+public interface IValidationRule
 {
-    public interface IValidationRule
-    {
-        /// <summary>
-        /// Метод для получения списка результатов проверки объекта CIM
-        /// </summary>
-        /// <param name="modelObject">Объект CIM из фрагмента</param>
-        /// <returns>Список результатов проверки объекта CIM</returns>
-        IEnumerable<ValidationResult> Execute(IModelObject modelObject);
-    }
+    /// <summary>
+    /// Метод для получения списка результатов проверки объекта CIM
+    /// </summary>
+    /// <param name="modelObject">Объект CIM из фрагмента</param>
+    /// <returns>Список результатов проверки объекта CIM</returns>
+    IEnumerable<IValidationResult> Execute(IReadOnlyModelObject modelObject);
+
+    ///
+    bool NeedExecute(IReadOnlyModelObject modelObject);
 }
