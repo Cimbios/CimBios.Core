6d6b490989abf22b3ed4c082c575763620656e7d
belizahrt <belizahrt@gmail.com>
2025-05-25 22:40:25 +1000

    ui: new controls background work ui blocking dialog cim model object properties observer control

---
+++ src/Tools/CimBios.Tools.ModelDebug/Views/CimObjectPropertiesObserver.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CimBios.Tools.ModelDebug.Views;
+
+public partial class CimObjectPropertiesObserver : UserControl
+{
+    public CimObjectPropertiesObserver()
+    {
+        InitializeComponent();
+    }
+}
