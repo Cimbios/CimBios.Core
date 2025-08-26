105ce068cd922112567174640bd788c0e2081119
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-08-26 14:57:25 +1000

    feat: validation results collection removed
    can be returned w/ script result

--- src/Core/CimModel/Validation/Script/ScriptGlobals.cs
+++ src/Core/CimModel/Validation/Script/ScriptGlobals.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CimBios.Core.CimModel.CimDatatypeLib;
 
 namespace CimBios.Core.CimModel.Validation.Script
@@ -10,7 +5,5 @@ namespace CimBios.Core.CimModel.Validation.Script
     public class ScriptGlobals
     {
         public IReadOnlyModelObject? ModelObject;
-
-        public IEnumerable<IValidationResult> ValidationResults = [];
     }
 }
