ddfbf82eaf58e47cdae9653953f27decac745af4
belizahrt <53006934+belizahrt@users.noreply.github.com>
2025-08-26 14:56:29 +1000

    minor reformat

--- src/Core/CimModel/Validation/Script/AssemblyInfo.cs
+++ src/Core/CimModel/Validation/Script/AssemblyInfo.cs
@@ -1,34 +1,26 @@
-﻿using CimBios.Core.CimModel.CimDatatypeLib;
-using CimBios.Core.CimModel.CimDatatypeLib.CIM17Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using CimBios.Core.CimModel.CimDatatypeLib;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CimBios.Core.CimModel.Validation.Script
+namespace CimBios.Core.CimModel.Validation.Script;
+public static class AssemblyInfo
 {
-    public class AssemblyInfo
-    {
-        public static Assembly[] References { get; } =
-        [
-            typeof(object).Assembly,
-            typeof(Uri).Assembly,
-            typeof(Enumerable).Assembly,
-            typeof(IModelObject).Assembly,
-            typeof(ModelObject).Assembly,
-            typeof(ValidationRuleBase).Assembly
-        ];
+    public static Assembly[] References { get; } =
+    [
+        typeof(object).Assembly,
+        typeof(Uri).Assembly,
+        typeof(Enumerable).Assembly,
+        typeof(IModelObject).Assembly,
+        typeof(ModelObject).Assembly,
+        typeof(ValidationRuleBase).Assembly
+    ];
 
-        public static string[] Usings { get; } =
-        [
-            "System",
-            "System.Collections.Generic",
-            "System.Linq",
-            "CimBios.Core.CimModel.CimDatatypeLib",
-            "CimBios.Core.CimModel.Validation",
-            "CimBios.Core.CimModel.CimDatatypeLib.CIM17Types"
-        ];
-    }
+    public static string[] Usings { get; } =
+    [
+        "System",
+        "System.Collections.Generic",
+        "System.Linq",
+        "CimBios.Core.CimModel.CimDatatypeLib",
+        "CimBios.Core.CimModel.Validation",
+        "CimBios.Core.CimModel.CimDatatypeLib.CIM17Types"
+    ];
 }
